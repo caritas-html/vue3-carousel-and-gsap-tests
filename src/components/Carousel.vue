25e4d6b66131a30bb1fcb1fb326848c570d90dc5
Leonardo Lima <leoexori@hotmail.com>
2021-12-23 16:19:31 -0300

    final tests

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,34 +1,32 @@
-<template >
-<div>
-  
-<div class="container">
-   <!-- <carousel :wrap-around="true">  -->
-    <!-- <slide v-for="slide in pokemons[0]" :key="slide"> -->
-
-    <div class="module__wrapper carousel__item">
-  <!-- <transition-group
-    @enter="carouselLogic"
-  > -->
-      <ul v-for="pokemon in pokemons[0]" :key="pokemon.name" class="module__slider" ref="pokemons">
-      
-        <li :key="pokemon.name">
+ <template>
+    
+  <Carousel
+     :wrap-around="true"
+     :itemsToShow="1.8"
+     :mouse-drag="false" 
+     :touch-drag="true" 
+     :settings="settings">
+    <Slide 
+    v-for="pokemon in pokemons[0]" 
+    :key="pokemon.name" 
+    class="module__slider"
+    >
+        <div :key="pokemon.name" class="carousel__item">
           {{pokemon.name}}
-        </li>
-      </ul>
-  <!-- </transition-group> -->
-    </div>
-    <!-- </slide> -->
-  <!-- </carousel>  -->
-</div>
+        </div>
+    </Slide>
+    <template #addons="{ currentSlide }">
+    <Navigation v-if="currentSlide" />
+    </template>
+  </Carousel>
   <button @click="spin">
     Spin
   </button>
-</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, onMounted, onBeforeMount, ref } from "vue";
-import { Carousel, Slide } from "vue3-carousel";
+import { defineComponent, reactive, onMounted, ref } from "vue";
+import { Carousel, Navigation, Slide } from "vue3-carousel";
 import { gsap } from 'gsap';
 
 import 'vue3-carousel/dist/carousel.css';
@@ -37,22 +35,13 @@ import 'vue3-carousel/dist/carousel.css';
 export default defineComponent({
   name: "carousel",
   components: {
-    // Carousel,
-    // Slide,
+    Carousel,
+    Slide,
+    Navigation,
   },
   setup() {
     let pokemons: any = reactive([]) 
-    let arr: string[] = []
     
-    const sizeList = (): string[] => {
-      let number = 0
-      for (let i = 0; i < 16; i++) {
-        number = number - 295
-        arr.push(number + 'px')
-      }
-      console.log(arr)
-      return arr
-    } 
 
     const fetchApi = async () => {
       await fetch("https://pokeapi.co/api/v2/pokemon", {
@@ -64,15 +53,12 @@ export default defineComponent({
       .then(res => pokemons.push(res))
     }
 
-    onBeforeMount(() => {
-      sizeList()
-    })
-
     onMounted(() => {
       fetchApi()
       })
 
-    let rangeValue = gsap.utils.mapRange(15, 0, -4500, 0);
+    let rangeValue = gsap.utils.mapRange(0, 15, 0, -1675.33)
+    let rangeValueTarget = gsap.utils.mapRange(0, -1675, 0, 15)
     let randomRangeValue = ref(Math.round(Math.random() * 15))
 
     // try to build a carousel logic //
@@ -80,7 +66,7 @@ export default defineComponent({
       gsap.to(el, {
       duration: 4,
       x: `${rangeValue(14)}px`,
-      ease: 'elastic.out(1, 0.2)',
+      ease: 'bounce.out(1000)',
       delay: 0.3,
       onComplete: done,
      })
@@ -93,28 +79,75 @@ export default defineComponent({
     }
   },
   methods: {
-    spin() {
-      gsap.to(".module__slider", {
+    numberOrder() {
+      const nodes = this.$el.nextElementSibling.firstChild.firstChild.childNodes
+      return nodes[Math.round(Math.random() * 14)].style.order
+    },
+     spin() {
+      const nodes = this.$el.nextElementSibling.firstChild.firstChild.childNodes
+      let nOrder = this.numberOrder()
+      let orderActive;    
+      
+      gsap.from(".carousel__track", {
+        x: `${this.rangeValue(Math.round(Math.random() * 14))}`,
+        onComplete: () => {
+          for (let i = 0; i < nodes.length; i++) {
+            if (nodes[i].classList.contains("carousel__slide--active")) {
+              nodes[i].style.order = 12;
+              nodes[i].classList.remove("carousel__slide--prev")
+            }
+            if (nodes[i].classList.contains("carousel__slide--prev")) {
+              nodes[i].style.order = parseInt(nOrder) - 1
+            }
+            if (nodes[i].classList.contains("carousel__slide--next")) {
+              nodes[i].style.order = parseInt(nOrder) + 1
+            }
+          }
+        }
+      })
+
+      gsap.to(".carousel__track", {
         duration: 4,
-        x: `${this.rangeValue(Math.round(Math.random() * 15))}px`,
+        x: function(index, target, targets) {
+          console.log(targets)
+          return targets[12]
+        },
         ease: 'elastic.out(1, 0.2)',
         delay: 0.3,
-     })
+      })
+    //   gsap.to(".carousel__slide--prev", {
+    //     rotation: -8,
+    //     y: 27,
+    //     x: -27,
+    //   })
+
+    //   gsap.to(".carousel__slide--next", {
+    //     rotation: 8,
+    //     y: 27,
+    //     x: -27,
+    //   })
+      // let result = true
+      
+      // if (this.result) {
+      //   console.log(!this.result)
+      //   this.result = true
+      //   return !this.result
+      // } else {
+      //   return true
+      // }
+    },
+  },
+  data: () => ({
+    result: true,
+    settings: {
+     snapAlign: 'center',
+     
     }
-  }
+  })
 })
 </script>
 
 <style>
-  .container {
-    display: flex;
-    justify-content: flex-start;
-    flex-direction: row;
-    width: 4500px;
-/* 9000px - 300px */
-    z-index: -1;
-  }
-
   .module__slider {
     width: fit-content;
     display: flex;
@@ -125,18 +158,21 @@ export default defineComponent({
     margin: 0 5px;
   }
 
-  .module__wrapper {
-    height: 350px;
-    width: 4500px;
-    flex-direction: row;
-    display: flex;
-    justify-content: center;
-    overflow: hidden;
+  .carousel__slide--visible {
+    order: 12
   }
 
-  .pokemon {
-
+  .carousel__slide--prev {
+    transform: rotate(352deg) translateY(27px) translateX(-27px);
   }
+
+  .carousel__slide--next {
+    transform: rotate(8deg) translateY(27px) translateX(27px);
+  }
+
+  .carousel__track {
+    width: 356px;
+  } 
 
   ul {
     padding: 0;
